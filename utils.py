56e4dbb884ee705731375cb276bb1428c1d47861
OscarGB <oscar.gomez.borz@gmail.com>
2020-10-05 16:57:13 +0200

    Unified script

--- utils.py
+++ utils.py
@@ -1,9 +1,14 @@
-import pandas as pd
-import numpy as np
-
-def load_dataset(file, columns, threshold):
-	df = pd.read_csv(file, skiprows=3, header=None, names=columns, dtype=np.float, sep='\s+', decimal=',', index_col=False)
-	for a in columns:
-		df[a+'_bin'] = 0
-		df.loc[df[a] > threshold, a+'_bin'] = 1
-	return df
+def get_spikes(df, col_name, t_ini=0.3, t_fin=0.1):
+    col = df[col_name]
+    to_col = col_name + "_bin"
+    df[to_col] = 0
+    i_pico = -1
+    for i in range(len(col)):
+        if i_pico < 0:
+            if col[i] >= t_ini:
+                i_pico = i
+        else:
+            if col[i] <= t_fin:
+                imax = col[i_pico:i].idxmax()
+                df.loc[imax, to_col] = 1
+                i_pico =-1
