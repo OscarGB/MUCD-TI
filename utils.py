2b85f42d2a67204dedf9393c1fe20f5fe9ffc0ff
Cabana <alejandro.cabana@estudiante.uam.es>
2020-10-13 16:50:21 +0200

    utils relleno

--- utils.py
+++ utils.py
@@ -13,9 +13,21 @@ def get_spikes(df, col_name, t_ini=0.3, t_fin=0.1):
                 df.loc[imax, to_col] = 1
                 i_pico =-1
 
-def shanon_entropy(X, w):
-    p = X.rolling(w).apply(lambda x: int(''.join(map(lambda x: str(int(x)), x)), 2)).value_counts()
+def resolution(X, n):
+    return X.groupby(X.index // n).max()
+
+def shanon_entropy(X, wlen):
+    p = X.rolling(wlen).apply(lambda x: int(''.join(map(lambda x: str(int(x)), x)), 2)).value_counts()
     p /= p.sum()
     return -np.sum(p*np.log2(p))
     
-    
+def shanon_entropy_join(X, wlen):
+    p = X.rolling(wlen).apply(lambda x: int(''.join(map(lambda x: str(int(x)), x)), 2))
+    p = p.dropna()
+    p["joined"] = p.values.tolist()
+    p = p.joined.value_counts()
+    p /= p.sum()
+    return -np.sum(p*np.log2(p))
+
+def mutual_info(X,cols,wlen):
+    return shanon_entropy(X[[cols[0]]],wlen) + shanon_entropy(X[[cols[1]]],wlen) - shanon_entropy_join(X[cols],wlen)
